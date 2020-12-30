a5b8bf200a75e67dd49bc49a0f50a6cda355b849
jvinueza <69803393+jvinueza@users.noreply.github.com>
2020-12-29 21:18:15 -0500

    Add files via upload

---
+++ l10n_ec/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# List of contributors:
+# Javier Vinueza <jvinueza@gmail.com>
+
+{
+    "name" : "Ecuador - Accounting (PGCE 2019)",
+    "version" : "12.0.1.2",
+    "author" : "Localización ecuatoriana",
+    'category': 'Localization',
+    "description": """Ecuador Plan de Cuentas 2019.""",
+    "depends" : [
+        "account",
+    ],
+    "data" : [
+        'data/account_group.xml',
+        'data/account_chart_template_data.xml',
+        'data/account.account.template-common.csv',
+        'data/account.account.template-sri.csv',
+        'data/account.account.template-cia.csv',
+        'data/account_chart_template_account_account_link.xml',
+        'data/account_data.xml',
+        'data/account_tax_data.xml',
+        'data/account_fiscal_position_template_data.xml',
+        'data/account_chart_template_configure_data.xml',
+    ],
+}
